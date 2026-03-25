988d57524b01f51a3345b7406ffb08bf643eeff8
Hopegeissler <hopegeiss043@gmail.com>
2026-03-25 11:08:18 +0000

    correcting sundrop elixir health- needs to be 4 per card rules

--- app/events/TileClicked.java
+++ app/events/TileClicked.java
@@ -373,7 +373,7 @@ if (gameState.selectedCardIsUnit) {
                     out,
                     gameState,
                     target,
-                    5,
+                    4,
                     null,
                     true,
                     StaticConfFiles.f1_buff
